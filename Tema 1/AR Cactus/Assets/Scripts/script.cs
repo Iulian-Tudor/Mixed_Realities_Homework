a7c54dd181a0a41ffc6c8c7f1360c2941fd13163
Iulian-Tudor <itscutaru@gmail.com>
2023-10-10 20:45:02 +0300

    Tema fara bonus, yet

---
+++ Tema 1/AR Cactus/Assets/Scripts/script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CactiAnimationController : MonoBehaviour
+{
+    public Transform otherCacti; // The other cacti to check distance with
+    [Range(0.1f, 10f)] // This makes attackDistance adjustable in the Unity Inspector
+    public float attackDistance = 1.0f; // The distance at which to start attacking
+    public Animator animator; // The animator controlling the cacti's animations
+    private bool isAttacking = false; // Variable to keep track of whether the cacti are attacking
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, otherCacti.position);
+
+        if (distance <= attackDistance && !isAttacking)
+        {
+            animator.SetBool("Attack", true);
+            isAttacking = true;
+        }
+        else if (distance > attackDistance && isAttacking)
+        {
+            animator.SetBool("Attack", false);
+            isAttacking = false;
+        }
+    }
+}
