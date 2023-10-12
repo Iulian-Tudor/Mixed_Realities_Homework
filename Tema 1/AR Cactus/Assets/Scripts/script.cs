6deec94cf20b301383e552b1988eaa5ab4a7c6ea
Iulian-Tudor <itscutaru@gmail.com>
2023-10-12 19:24:22 +0300

    Added sound

--- Tema 1/AR Cactus/Assets/Scripts/script.cs
+++ Tema 1/AR Cactus/Assets/Scripts/script.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 
-public class CactiAnimationController : MonoBehaviour
+public class script : MonoBehaviour
 {
     public Transform otherCacti; // The other cacti to check distance with
     [Range(0.1f, 10f)] // This makes attackDistance adjustable in the Unity Inspector
     public float attackDistance = 1.0f; // The distance at which to start attacking
     public Animator animator; // The animator controlling the cacti's animations
     private bool isAttacking = false; // Variable to keep track of whether the cacti are attacking
+    public AudioSource audioSource; // Variable for the sound clip of punching
 
     void Update()
     {
         float distance = Vector3.Distance(transform.position, otherCacti.position);
+
+        transform.LookAt(otherCacti);
 
         if (distance <= attackDistance && !isAttacking)
         {
@@ -22,5 +25,19 @@ public class CactiAnimationController : MonoBehaviour
             animator.SetBool("Attack", false);
             isAttacking = false;
         }
+
+    }
+
+    public void PlayPunchSound()
+    {
+        if (isAttacking)
+        {
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop(); 
+        }
+        
     }
 }
