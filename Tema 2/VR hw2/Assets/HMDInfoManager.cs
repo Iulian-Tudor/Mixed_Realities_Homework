5d73e6e6229c7eba690b7c400bbdab226afe2286
Stirbu.Larisa <eusuntlarisa@gmail.com>
2023-10-17 13:50:26 +0300

    Tema 2

---
+++ Tema 2/VR hw2/Assets/HMDInfoManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HMDInfoManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!XRSettings.isDeviceActive)
+        {
+            Debug.Log("no headset");
+        }
+
+        else if (XRSettings.isDeviceActive &&
+                 (XRSettings.loadedDeviceName == "Mock HMD" || XRSettings.loadedDeviceName == "MockHMD Display"))
+        {
+            Debug.Log("using mock hmd");
+        }
+        else
+        {
+            Debug.Log("headset detected");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
